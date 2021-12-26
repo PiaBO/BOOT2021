d56de36c4e77cb041eb41cec496df6c55dee18b6
PiaBO <mpiabarcelo@gmail.com>
2021-12-26 16:21:12 +0100

    ejercicios js

---
+++ fundamentos/ejercicios.js
@@ -0,0 +1,70 @@
+function random(){
+    let range = prompt("Dime rango:")
+
+    alert(Math.floor(Math.random() * range)); 
+}
+
+function adivinarNumero(){
+    let num = Math.floor(Math.random() * 100);
+    var attempt = 10;
+
+    while(attempt){
+        let userNum = prompt("Dime un número: ");
+        if (userNum == num) {
+            return alert("Correcto el número era "+num);
+        } else if(userNum > num){
+            alert("El número es menor");
+        }else{
+            alert("El número es mayor");
+        }
+        attempt--;
+    }
+
+    return alert("Has perdido")
+}
+
+function pedirArray(){
+    let len = prompt("Length: ");
+    let val = prompt("Value: ");
+    createArray(len,val);
+}
+
+function createArray(length, value){
+    let array = [];
+    array.length = length;
+    array.fill(value);
+    alert(array);
+}
+function pedirPrimos(){
+    let num = prompt("Di un numero: ");
+    primo(num);
+}
+
+function primo(num){
+    let array = [];
+    for (let index = 0; index < num; index++) {
+        if(comprobarPrimo(index) === true)
+            array.push(index);
+    }    
+
+    alert(array);
+}
+
+function comprobarPrimo(num){
+    for (let index = 2; index < num; index++) {
+        if(num % index === 0)
+            return false;
+    }
+    return true;
+}
+
+function validarDNI(){
+    let dni = prompt("dni: ");
+    let expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
+
+    if(expresion_regular_dni.test(dni)){
+        alert("correcto");
+    }else{
+        alert("incorrecto");
+    }
+}
