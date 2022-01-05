c765f4b278f2942f7b0ec063c1841373f0a995be
Pia Barcelo <mpiabarcelo@gmail.com>
2022-01-05 17:26:13 +0100

    Clase calculadora

--- fundamentos/calculadora/calculadora.js
+++ fundamentos/calculadora/calculadora.js
@@ -1,94 +1,112 @@
-var resultadoPlaceholder;
-var operacionTerminada;
 
-function getElement(item){
+function getElement(item) {
     return document.querySelector(item);
 }
-function getElements(item){
-    return document.querySelectorAll(item);
+function getElements(item) {
+return document.querySelectorAll(item);
 }
 
-function limpiar(){
-    getElement("#resultado").innerHTML = "";
-    getElement("#acumulado").innerHTML = "";
+function getResultado(){
+    return getElement("#resultado");
+}
+function getAcumulado(){
+    return getElement("#acumulado");
 }
 
-function teclaNumero(num){
-    if(resultadoPlaceholder){
-        resultadoPlaceholder = false;
-        if(operacionTerminada){
-            limpiar();
-            operacionTerminada = false;
+class Calculadora {
+    #resultadoPlaceholder;
+    #operacionTerminada;
+
+    constructor(){
+        this.#resultadoPlaceholder = false;
+        this.#operacionTerminada = false;
+        this.limpiar();
+    }
+    
+    limpiar() {
+        getResultado().innerHTML = "";
+        getAcumulado().innerHTML = "";
+    }
+
+    igual() {
+        if(!this.#operacionTerminada){
+            let acumulado = getAcumulado().innerHTML;
+            let arrOperacion = this.separador(acumulado);
+            if (arrOperacion.length > 1) {
+                let operacion = getAcumulado().innerHTML+ getResultado().innerHTML;
+                getAcumulado().innerHTML += getResultado().innerHTML+ "=";
+                let result = this.calcularResultado(operacion);
+                getResultado().innerHTML = result;
+            }
         }
-        getElement("#resultado").innerHTML = num;
-    }else{
-        getElement("#resultado").innerHTML += num;
+        this.#operacionTerminada = true;
+        this.#resultadoPlaceholder = true;
     }
-}
-
-function teclaOperacion(signoOperacion){
-    let acumulado = getElement("#acumulado").innerHTML;
-
-    if(acumulado.search("=")!=-1){
-       getElement("#acumulado").innerHTML = getElement("#resultado").innerHTML + signoOperacion;
-       resultadoPlaceholder = true;
-       operacionTerminada = false;
-    }else{
-        let operacion = acumulado + getElement("#resultado").innerHTML;
-        let arrOperacion = separador(operacion);
-        resultadoPlaceholder = true;
-        if(arrOperacion.length > 1){
-            let result = Math.round(eval(operacion.replace(",",".")) * 100) / 100;
-            getElement("#resultado").innerHTML = new String(result).replace(".",",");
-            getElement("#acumulado").innerHTML = getElement("#resultado").innerHTML + signoOperacion;
-
-            operacionTerminada = false;
-        }else{
-            getElement("#acumulado").innerHTML += getElement("#resultado").innerHTML + signoOperacion;
-        }        
-    }
-
-
-}
-
-function igual(){
-    if(!operacionTerminada){
-        let acumulado = getElement("#acumulado").innerHTML;
-        let arrOperacion = separador(acumulado);
-        if(arrOperacion.length > 1){
-            let operacion = getElement("#acumulado").innerHTML + getElement("#resultado").innerHTML;
-            getElement("#acumulado").innerHTML += getElement("#resultado").innerHTML + "=";
-            let result = Math.round(eval(operacion.replace(",",".")) * 100) / 100;
-            getElement("#resultado").innerHTML = new String(result).replace(".",",");
+    teclaNumero(num) {
+        if (this.#resultadoPlaceholder) {
+            this.#resultadoPlaceholder = false;
+            if (this.#operacionTerminada) {
+                limpiar();
+                this.#operacionTerminada = false;
+            }
+            getResultado().innerHTML = num;
+        } else {
+            getResultado().innerHTML+= num;
         }
-        operacionTerminada = true;
-        resultadoPlaceholder = true;
-    }else{
-        
     }
+
+    teclaOperacion(signoOperacion) {
+        let acumulado = getAcumulado().innerHTML;
+        if (acumulado.search("=") != -1) {
+            getAcumulado().innerHTML = getResultado().innerHTML+ signoOperacion;
+            this.#resultadoPlaceholder = true;
+            this.#operacionTerminada = false;
+        } else {
+            let operacion = acumulado + getResultado().innerHTML;
+            let arrOperacion = this.separador(operacion);
+            this.#resultadoPlaceholder = true;
+            if (arrOperacion.length > 1) {
+                let result = this.calcularResultado(operacion);
+                getResultado().innerHTML = result;
+                getAcumulado().innerHTML = result + signoOperacion;
+                this.#operacionTerminada = false;
+            } else {
+                getAcumulado().innerHTML+= getResultado().innerHTML+ signoOperacion;
+            }
+        }
+    }
+
+
+    separador(str) {
+        let arry = new String(str.split('+').join(', ').split('-').join(', ').split('/').join(', ').split('*').join(', ').split('='));
+        return arry.split(",");
+    }
+
+    calcularResultado(num){
+        let result = Math.round(eval(num.replace(",", ".")) * 100) / 100;
+        return  new String(result).replace(".", ",");
+    }
+    borrar() {
+        let resultado = getResultado().innerHTML;
+        getResultado().innerHTML = resultado.substr(0, resultado.length - 1)
+    }
+    
 }
 
-function separador(str){
-    let arry = new String(str.split('+').join(', ').split('-').join(', ').split('/').join(', ').split('*').join(', ').split('='));
-    return arry.split(",");
-}
-
-function borrar(){
-    let resultado = getElement("#resultado").innerHTML;
-    getElement("#resultado").innerHTML = resultado.substr(0,resultado.length-1)
-}
-
-window.onload=function(){
-    resultadoPlaceholder = false;
-    operacionTerminada = false;
-    limpiar();
-    getElements(".num").forEach(btn => btn.addEventListener('click', function(ev){
-        teclaNumero(ev.target.value);
+window.onload = function () {
+    var calculadora = new Calculadora();
+    getElements(".num").forEach(btn => btn.addEventListener('click', function (ev) {
+        calculadora.teclaNumero(ev.target.value);
     }));
-    getElements(".operation").forEach(btn => btn.addEventListener('click', function(ev){
-        teclaOperacion(ev.target.value);
+    getElements(".operation").forEach(btn => btn.addEventListener('click', function (ev) {
+        calculadora.teclaOperacion(ev.target.value);
     }));
-    getElement("#limpiar").addEventListener('click', limpiar);
-    getElement("#igual").addEventListener('click', igual);
-    getElement("#borrar").addEventListener('click', borrar);
+    getElement("#limpiar").addEventListener('click', calculadora.limpiar);
+    getElement("#igual").addEventListener('click', 
+        function(){
+            calculadora.igual();
+        }
+    
+    );
+    getElement("#borrar").addEventListener('click', calculadora.borrar);
 }
