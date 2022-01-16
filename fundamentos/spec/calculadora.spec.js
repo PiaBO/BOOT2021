1f9fd74019c09c12be05d94c89f5b1f624c94efb
PiaBO <mpiabarcelo@gmail.com>
2022-01-16 13:18:48 +0100

    Selenium

--- fundamentos/spec/calculadora.spec.js
+++ fundamentos/spec/calculadora.spec.js
@@ -29,18 +29,7 @@ describe('Test teclas',()=>{
             expect(calculadora.acumulado).toBe(number+"+");
         });
     }) 
-    it('Probamos cambio de signo ', function(){
-        let num = ['1','2','3','4','5','6','7','8','9','0'];
-        let contador = '';
-        
-        num.forEach(number => {
-            calculadora.teclaNumero(number);
-            contador+=number;
-            calculadora.cambioSigno();
-            expect(calculadora.resultado).toBe(-contador);
-            contador = -contador;
-        });
-    }) 
+
 });
 describe('Test operaciones',()=>{
     let num = ['1021','2021','30.21','4021','5021','60.21','0.7','128','569','0.045'];
@@ -76,33 +65,48 @@ describe('Test operaciones',()=>{
 });
 describe('Test teclas especiales',()=>{
     let num = ['1021','2021','30.21','4021','5021','60.21','0.7','128','569','0.045'];
+    it('Borrar ', function(){
+        num.forEach(number => {
+            calculadora.teclaNumero(number);
+            calculadora.borrar();
+            expect(calculadora.resultado).toBe(number.substring(0,number.length-1));
+            calculadora.limpiar();
+        });
+    }) 
+    it('Limpiar', function(){
+        calculadora.teclaNumero("123");
+        calculadora.limpiar();
+        expect(calculadora.resultado).toBe("0");
+    }) 
+    it('Probamos cambio de signo ', function(){
+        let num = ['1','2','3','4','5','6','7','8','9','0'];
+        let contador = '';
+        
+        num.forEach(number => {
+            calculadora.teclaNumero(number);
+            contador+=number;
+            calculadora.cambioSigno();
+            expect(calculadora.resultado).toBe(-contador);
+            contador = -contador;
+        });
+    }) 
+});
 
-    it('Sumas ', function(){
-        num.forEach(number => {
-            let operacion = number+"+"+number;
-            calculadora.operador = "+";
-            expect(calculadora.calcular(operacion)).toBe(eval(operacion));
-        });           
+describe('Test decimales',()=>{
+    let num = ['1021','2021','30.21','4021','5021','60.21','0.7','128','569','0.045'];
+    it('Decimales ', function(){
+        calculadora.coma();
+        expect(calculadora.resultado).toBe("0,");
+        calculadora.coma();
+        expect(calculadora.resultado).toBe("0,");
+        calculadora.coma();
+        calculadora.teclaOperacion("+");
+        expect(calculadora.resultado).toBe("0,0");
     }) 
-    it('Restas ', function(){
-    num.forEach(number => {
-        let operacion = number+"-"+number;
-        calculadora.operador = "-";
-        expect(calculadora.calcular(operacion)).toBe(eval(operacion));
-    });           
-        }) 
-    it('Multiplicaciones ', function(){
-        num.forEach(number => {
-            let operacion = number+"*"+number;
-            calculadora.operador = "*";
-            expect(calculadora.calcular(operacion)).toBe(eval(operacion));
-        });           
+    it('Limpiar', function(){
+        calculadora.teclaNumero("123");
+        calculadora.limpiar();
+        expect(calculadora.resultado).toBe("0");
     }) 
-    it('Divisiones ', function(){
-        num.forEach(number => {
-            let operacion = number+"/"+number;
-            calculadora.operador = "/";
-            expect(calculadora.calcular(operacion)).toBe(eval(operacion));
-        });           
-    }) 
+
 });
